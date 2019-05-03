229bdb06ac196b8cf697ddd40085b536a8026816
Germanov,Ivan I.P <i.germanov@student.fontys.nl>
2019-05-03 14:02:45 +0200

    Added issue service types

--- src/components/IssuesComponent.vue
+++ src/components/IssuesComponent.vue
@@ -6,12 +6,22 @@
       <div v-if="!showLoaderIssues">
         <div class="issue" v-for="(issue, i) in this.issues" :key="i">
           <div class="title">{{issue.title}}</div>
-          <div class="date">
-            <span>{{issue.createdAt | date}}</span>
-            <span
-              class="updated"
-              v-if="issue.createdAt != issue.updatedAt"
-            >, updated at {{issue.updatedAt | date}}</span>
+          <div>
+            <span class="date">
+              <span>{{issue.createdAt | date}}</span>
+              <span
+                class="updated"
+                v-if="issue.createdAt != issue.updatedAt"
+              >, updated at {{issue.updatedAt | date}}</span>
+            </span>
+            <span>
+              <a
+                class="serviceType"
+                :class="serviceType"
+                v-for="(serviceType, j) in issue.serviceTypes"
+                :key="j"
+              >{{serviceType}}</a>
+            </span>
           </div>
           <vue-markdown class="body">{{issue.body}}</vue-markdown>
         </div>
@@ -166,6 +176,39 @@ export default class IssuesComponent extends Vue {
 </script>
 
 <style lang="scss">
+.serviceType {
+  border-radius: 2px;
+  box-shadow: inset 0 -1px 0 rgba(27, 31, 35, 0.12);
+  font-size: 12px;
+  font-weight: 600;
+  margin: 5px;
+  height: 20px;
+  line-height: 15px;
+  padding: 0.15em 4px;
+  display: inline-block;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+.cloud {
+  color: #000000;
+  background-color: #a2eeef;
+}
+
+.cell_counter {
+  color: #ffffff;
+  background-color: #7057ff;
+}
+
+.lux_2 {
+  background-color: #e4e669;
+  color: #000000;
+}
+
+.omni {
+  color: #ffffff;
+  background-color: #d73a4a;
+}
+
 .relative {
   position: relative;
 }
